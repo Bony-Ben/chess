945212e0219ced7792f82f4d4b5fc02145555e6b
Tony Chen <t349chen@uwaterloo.ca>
2021-12-05 20:44:40 -0500

    Temporary Fix: Made graphics observer redraw entire board every time after being notified.  Permanent solution involves making notify accept move as an argument and using that information to reduce redrawing.

--- display/graphicsobserver.cc
+++ display/graphicsobserver.cc
@@ -5,6 +5,15 @@
 
 GraphicsObserver::GraphicsObserver(Board *board) : board{board} {
     w = new Xwindow(720, 720);
+    board->attach(this);
+}
+
+GraphicsObserver::~GraphicsObserver() {
+    delete w;
+    board->detach(this);
+}
+
+void GraphicsObserver::notify() {
     bool white = true;
     for (int y = 0; y <= 630; y += 90) {
         for (int x = 0; x <= 630; x += 90) {
@@ -19,23 +28,14 @@ GraphicsObserver::GraphicsObserver(Board *board) : board{board} {
     }
     int num_label = 8;
     char alph_label = 'A';
-    for (int y = 45; y <= 640; y += 180)  {
+    for (int y = 45; y <= 640; y += 180) {
         w->drawString(10, y, std::to_string(num_label), Xwindow::Black);
-        w->drawString(10, y + 90, std::to_string(num_label-1), Xwindow::White);
+        w->drawString(10, y + 90, std::to_string(num_label - 1), Xwindow::White);
         w->drawString(y, 710, std::string(1, alph_label), Xwindow::White);
         w->drawString(y + 90, 710, std::string(1, alph_label + 1), Xwindow::Black);
         num_label -= 2;
         alph_label += 2;
     }
-    board->attach(this);
-}
-
-GraphicsObserver::~GraphicsObserver() {
-    delete w;
-    board->detach(this);
-}
-
-void GraphicsObserver::notify() {
     std::vector<Piece *> pieces = board->getPieces();
     for (auto p : pieces) {
         if ((p->getRank() % 2) != (p->getFile() % 2)) {
