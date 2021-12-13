04b4641d0c80673bed46eb23b94c30b53705b5c7
Julia Chen <juliazychen@gmail.com>
2021-12-13 14:48:40 -0500

    command interpreter for level3.cc

--- players/level3.cc
+++ players/level3.cc
@@ -7,15 +7,27 @@
 #include <vector>
 
 #include "../board.h"
+#include "../inputparser.h"
 #include "../move.h"
 #include "human.h"
 
 void Level3::makeMove(Board &board, std::vector<Move> &moves, char colour) {
     std::string s;
-    while (std::cin >> s) {
-        if (s == "move") {
-            board.makeMove(moves[rand() % moves.size()]);
-            return;
+    while (std::getline(std::cin, s)) {
+        try {
+            std::vector<std::string> input = parseLine(s);
+            if (input.size() == 0) {
+                throw InputException{};
+            }
+            s = input.at(0);
+            if (s == "move") {
+                board.makeMove(moves[rand() % moves.size()]);
+                return;
+            } else {
+                throw InputException{};
+            }
+        } catch (InputException i) {
+            std::cout << "Invalid Input! Please try again." << std ::endl;
         }
     }
     throw EndOfFileException{};
